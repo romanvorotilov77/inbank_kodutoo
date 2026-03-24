544c4293725c72cad97d5002abb4ee50fd2937ad
Your Name <rovoro@taltech.ee>
2026-03-24 18:02:11 +0200

    feat: Introduce LoanConstraints for loan amount and period limits

---
+++ backend/Constants/LoanConstraints.cs
@@ -0,0 +1,10 @@
+namespace backend.Constants
+{
+    public static class LoanConstraints
+    {
+        public const int MinAmount = 2000;
+        public const int MaxAmount = 10000;
+        public const int MinPeriod = 12;
+        public const int MaxPeriod = 60;
+    }
+}
