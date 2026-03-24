54d50d0a269a6c28cc4a886ea08293578377f20b
Your Name <rovoro@taltech.ee>
2026-03-24 17:57:29 +0200

    refactor: Change LoanRequest Amount from decimal to int for simplicity.

--- backend/Models/LoanRequest.cs
+++ backend/Models/LoanRequest.cs
@@ -3,7 +3,7 @@ namespace backend.Models
     public class LoanRequest
     {
        public required string PersonalCode { get; set; }
-       public decimal Amount { get; set; }
+       public int Amount { get; set; }
        public int LoanPeriod { get; set; }
     }
 }
